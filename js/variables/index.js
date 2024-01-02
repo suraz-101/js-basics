24e67baa5b978ed0758db6386dd7604c7255d55e
Suraj Pandey <surazpandey101@gmail.com>
2024-01-02 09:24:03 +0545

    learning variables

---
+++ js/variables/index.js
@@ -0,0 +1,35 @@
+console.log("Hellow World");
+
+//This is how you can comment in javascript
+
+/* this is how 
+   you can comment multiple ines 
+   in javascript 
+   
+   Read me instruction
+   a. Variables - named storage
+
+    => standard ECMASCRIPT (This is the standard in which javascript is done)
+
+    es5
+
+    At the early stage there was only var keywords
+
+    es6
+
+   let and  const were introduced alongside with var
+ 
+
+*/
+
+var person = "suraj"; //container = data
+
+person = "pandey";
+var fullName = "Suraj Pandey"; //camel case This is the right way to give naming convention
+var isAdult = "yes";
+
+let persons = "pandey";
+persons = "pandey suraj";
+//var and let allow their value to be replaced by another value but const doest not allow it
+
+console.log(person);
