4ce063fe38fdb009705c7470f10a03c7d83d4953
Suraj Pandey <surazpandey101@gmail.com>
2024-01-19 23:22:41 +0545

    practicing

--- carousel/slider.js
+++ carousel/slider.js
@@ -1,3 +1,5 @@
-setInterval(() => {}, 2000);
-
-const image = document.getElementsByClassName(".carousel-item img");
+const image = [
+  "https://images.assetsdelivery.com/compings_v2/ongkachakon/ongkachakon2011/ongkachakon201100026.jpg",
+  "https://www.newzealand.com/assets/Tourism-NZ/Wellington/bcab3eae5b/img-1536146521-3287-14997-p-2BB80D14-E659-C48E-BB5EC5B2906B7758-2544003__aWxvdmVrZWxseQo_FocalPointCropWzM1MiwxMDI0LDUwLDUwLDc1LCJwbmciLDY1LDIuNV0.png",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRenLnADWbObaYRGhPEW_3PINNJrCVM1_Z-0myxnoRE6flxurMmscysbrRYR21yActE_4U&usqp=CAU",
+];
